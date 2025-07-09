f2425fe8062c932c8301fc750eec7949a60e3af4
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-07-09 22:46:59 +0300

    toggle burger menu icon on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 function eventListener() {
   const burgerMenuElement = document.querySelector('.burger-menu');
-
   burgerMenuElement.addEventListener('click', onBurgerMenuClick);
 }
 
@@ -8,6 +7,23 @@ function onBurgerMenuClick() {
   const mainMenuContainerElement = document.querySelector(
     '.main-menu-container'
   );
+  const burgerMenuElement = document.querySelector('.burger-menu');
 
+  // Show mobile navigation
   mainMenuContainerElement.style.display = 'block';
+
+  // Toggle burger menu icon
+  if (burgerMenuElement.classList.contains('open-burger-menu')) {
+    burgerMenuElement.classList.replace(
+      'open-burger-menu',
+      'close-burger-menu'
+    );
+  } else {
+    burgerMenuElement.classList.replace(
+      'close-burger-menu',
+      'open-burger-menu'
+    );
+  }
 }
+
+eventListener();
