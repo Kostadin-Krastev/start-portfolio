c8c3f748e1215cde8c3ef576d7a9a12df9f5a3c2
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-07-09 21:35:44 +0300

    add function open burger menu content on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-const burgerMenuElement = document.querySelector(burger - menu);
+function eventListener() {
+  const burgerMenuElement = document.querySelector('.burger-menu');
+
+  burgerMenuElement.addEventListener('click', onBurgerMenuClick);
+}
+
+function onBurgerMenuClick() {
+  const mainMenuContainerElement = document.querySelector(
+    '.main-menu-container'
+  );
+
+  mainMenuContainerElement.style.display = 'block';
+}
